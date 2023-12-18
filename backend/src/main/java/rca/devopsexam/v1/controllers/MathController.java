a130a1b7b8374ea96773fe1b33735e82c35f1bc0
ritahhhh <joyeuserita@gmail.com>
2023-12-18 11:43:20 +0200

    Added final end to end tests

--- backend/src/main/java/rca/devopsexam/v1/controllers/MathController.java
+++ backend/src/main/java/rca/devopsexam/v1/controllers/MathController.java
@@ -1,23 +1,40 @@
-package rca.devopsexam.v1.controllers;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import rca.devopsexam.v1.dtos.DoMathRequestDto;
-import rca.devopsexam.v1.exceptions.InvalidOperationException;
-import rca.devopsexam.v1.payload.ApiResponse;
-import rca.devopsexam.v1.serviceImpls.MathOperatorImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/do_math")
 public class MathController {
-    private final MathOperatorImpl mathOperatorImpl;
 
-    public MathController(MathOperatorImpl mathOperatorImpl) {
-        this.mathOperatorImpl = mathOperatorImpl;
-    }
+    @PostMapping("/doMath")
+    public ResponseEntity<CalcResponse> doMath(@RequestBody DoMathRequest doMathRequest) {
+        double operand1 = doMathRequest.getOperand1();
+        double operand2 = doMathRequest.getOperand2();
+        String operation = doMathRequest.getOperation();
+        double result;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse> create(@RequestBody DoMathRequestDto dto) throws InvalidOperationException {
-        return ResponseEntity.ok(ApiResponse.success(mathOperatorImpl.doMath(dto.getOperand1(), dto.getOperand2(), dto.getOperation())));
+        switch (operation) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                if (operand2 == 0) {
+                    return ResponseEntity.badRequest().build(); // Return bad request for division by zero
+                }
+                result = operand1 / operand2;
+                break;
+            default:
+                return ResponseEntity.badRequest().build(); // Invalid operation
+        }
+
+        CalcResponse calcResponse = new CalcResponse(result);
+        return ResponseEntity.ok(calcResponse);
     }
 }
--- backend/src/test/java/rca/devopsexam/end-to-end/MathApplicationEndToEndTest.java
+++ backend/src/test/java/rca/devopsexam/end-to-end/MathApplicationEndToEndTest.java
@@ -1,59 +1,31 @@
 package rca.devopsexam.e2e;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class MathApplicationEndToEndTest {
-
+public class MathApplicationTestE2E {
     @LocalServerPort
     private int port;
-
-    private WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        // Set up WebDriver (assuming you have ChromeDriver installed)
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        // Close the browser after each test
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
+    @Autowired
+    private TestRestTemplate restTemplate;
     @Test
-    public void testDoMathOperation_endToEnd() {
+    public void testApiEndpoint() {
         // Given
-        driver.get("http://localhost:" + port + "/");  // Assuming your application is running at the root context
-
-        // Find input fields and submit button
-        WebElement operand1Input = driver.findElement(By.id("operand1"));
-        WebElement operand2Input = driver.findElement(By.id("operand2"));
-        WebElement operationInput = driver.findElement(By.id("operation"));
-        WebElement calculateButton = driver.findElement(By.id("calculateButton"));
-
+        String url = "http://localhost:" + port + "/api/v1/do_math";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String requestBody = "{\"operand1\": 2, \"operand2\": 5, \"operation\": \"+\"}";
         // When
-        operand1Input.sendKeys("9");
-        operand2Input.sendKeys("11");
-        operationInput.sendKeys("+");
-        calculateButton.click();
-
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+        String response = restTemplate.postForObject(url, request, String.class);
         // Then
-        WebElement resultElement = driver.findElement(By.id("result"));
-        String resultText = resultElement.getText();
-        assertEquals("20.0", resultText);
+        assertEquals("Expected Response", response);
     }
 }
